f17fed1a99e60c5ee125936f37e19c919b9cb901
Egii <regidesoftian@gmail.com>
2022-07-05 21:58:42 +0000

    Add extras for merlin-user 12 SP1A.210812.016 V13.0.1.0.SJOCNXM release-keys
    Signed-off-by: Egii <regidesoftian@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## merlin-user 12 SP1A.210812.016 V13.0.1.0.SJOCNXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6768
+- Codename: merlin
+- Brand: Redmi
+- Flavor: merlin-user
+- Release Version: 12
+- Id: SP1A.210812.016
+- Incremental: V13.0.1.0.SJOCNXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 440
+- Fingerprint: Redmi/merlin/merlin:12/SP1A.210812.016/V13.0.1.0.SJOCNXM:user/release-keys
+- OTA version: 
+- Branch: merlin-user-12-SP1A.210812.016-V13.0.1.0.SJOCNXM-release-keys
+- Repo: redmi_merlin_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cat cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null >> cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk
+rm -f cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk.* 2>/dev/null >> cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk
+rm -f cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null >> cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk
+rm -f cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.UCMobile_11/partner-com.UCMobile_11.apk.* 2>/dev/null >> cust/app/customized/partner-com.UCMobile_11/partner-com.UCMobile_11.apk
+rm -f cust/app/customized/partner-com.UCMobile_11/partner-com.UCMobile_11.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> system/system/data-app/SmartHome/SmartHome.apk
+rm -f system/system/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat system/system/data-app/MIUIVipAccount/MIUIVipAccount.apk.* 2>/dev/null >> system/system/data-app/MIUIVipAccount/MIUIVipAccount.apk
+rm -f system/system/data-app/MIUIVipAccount/MIUIVipAccount.apk.* 2>/dev/null
+cat system/system/data-app/MiuiScanner/MiuiScanner.apk.* 2>/dev/null >> system/system/data-app/MiuiScanner/MiuiScanner.apk
+rm -f system/system/data-app/MiuiScanner/MiuiScanner.apk.* 2>/dev/null
+cat system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null >> system/system/data-app/MiShop/MiShop.apk
+rm -f system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null
+cat system/system/data-app/com.autonavi.minimap_12/com.autonavi.minimap_12.apk.* 2>/dev/null >> system/system/data-app/com.autonavi.minimap_12/com.autonavi.minimap_12.apk
+rm -f system/system/data-app/com.autonavi.minimap_12/com.autonavi.minimap_12.apk.* 2>/dev/null
+cat system/system/data-app/MIUIYoupin/MIUIYoupin.apk.* 2>/dev/null >> system/system/data-app/MIUIYoupin/MIUIYoupin.apk
+rm -f system/system/data-app/MIUIYoupin/MIUIYoupin.apk.* 2>/dev/null
+cat system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system/system/system_ext/apex/com.android.vndk.v30.apex
+rm -f system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIVideo/MIUIVideo.apk.* 2>/dev/null >> system/system/priv-app/MIUIVideo/MIUIVideo.apk
+rm -f system/system/priv-app/MIUIVideo/MIUIVideo.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIGallery/MIUIGallery.apk.* 2>/dev/null >> system/system/priv-app/MIUIGallery/MIUIGallery.apk
+rm -f system/system/priv-app/MIUIGallery/MIUIGallery.apk.* 2>/dev/null
+cat system/system/priv-app/MiBrowser/MiBrowser.apk.* 2>/dev/null >> system/system/priv-app/MiBrowser/MiBrowser.apk
+rm -f system/system/priv-app/MiBrowser/MiBrowser.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIMusic/MIUIMusic.apk.* 2>/dev/null >> system/system/priv-app/MIUIMusic/MIUIMusic.apk
+rm -f system/system/priv-app/MIUIMusic/MIUIMusic.apk.* 2>/dev/null
+cat system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null >> system/system/app/VoiceAssist/VoiceAssist.apk
+rm -f system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null
+cat system/system/app/SogouInput/SogouInput.apk.* 2>/dev/null >> system/system/app/SogouInput/SogouInput.apk
+rm -f system/system/app/SogouInput/SogouInput.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
